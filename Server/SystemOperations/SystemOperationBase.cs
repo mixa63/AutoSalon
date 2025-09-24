8bf152d0f55ad00d4e8bb0fd284fd38cf1400586
mixa63 <183983866+mixa63@users.noreply.github.com>
2025-09-24 09:00:25 +0200

    Dodaj SystemOperationBase apstraktnu klasu

---
+++ Server/SystemOperations/SystemOperationBase.cs
@@ -0,0 +1,59 @@
+﻿using DBBroker;
+using ServerApp;
+
+namespace Server.SystemOperations
+{
+    /// <summary>
+    /// Apstraktna bazna klasa koja definise zajednicki template za izvrsavanje sistemskih operacija.
+    /// Obezbedjuje upravljanje konekcijom i transakcijama ka bazi podataka.
+    /// </summary>
+    internal abstract class SystemOperationBase
+    {
+        /// <summary>
+        /// Broker za rad sa bazom podataka.
+        /// </summary>
+        protected Broker broker;
+
+        /// <summary>
+        /// Konstruktor koji inicijalizuje broker sa connection string-om iz ConfigManager-a.
+        /// </summary>
+        internal SystemOperationBase()
+        {
+            broker = new Broker(ConfigManager.AppConfig.ConnectionString);
+        }
+
+        /// <summary>
+        /// Template metod koji upravlja celokupnim tokom izvrsavanja sistemske operacije.
+        /// Obuhvata otvaranje konekcije, pokretanje transakcije, izvrsavanje konkretne operacije,
+        /// potvrdu ili ponistavanje transakcije i zatvaranje konekcije.
+        /// </summary>
+        /// <exception cref="Exception">Baca izuzetak u slucaju greske tokom izvrsavanja operacije.</exception>
+        internal void ExecuteTemplate()
+        {
+            try
+            {
+                broker.OpenConnection();
+                broker.BeginTransaction();
+
+                ExecuteConcreteOperation();
+
+                broker.Commit();
+            }
+            catch (Exception ex)
+            {
+                broker.Rollback();
+                throw;
+            }
+            finally
+            {
+                broker.CloseConnection();
+            }
+        }
+
+        /// <summary>
+        /// Apstraktni metod koji konkretne klase moraju da implementiraju.
+        /// Sadrzi specifichnu logiku odredjene sistemske operacije.
+        /// </summary>
+        protected abstract void ExecuteConcreteOperation();
+    }
+}
