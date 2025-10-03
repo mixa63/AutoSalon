fb762b3c6c8a58a70e1791063f14bbeb669cfb16
mixa63 <183983866+mixa63@users.noreply.github.com>
2025-10-04 01:19:16 +0200

    Dodaj AppConfig klasu

---
+++ Client/AppConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class AppConfig
+    {
+        public string Ip { get; set; }
+        public int Port { get; set; }
+    }
+}
