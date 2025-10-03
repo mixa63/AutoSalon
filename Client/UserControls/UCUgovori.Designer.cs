d2e3e4cc58f0cdd63ccfe8aaaf2aeef2ff243e1c
mixa63 <183983866+mixa63@users.noreply.github.com>
2025-10-04 01:12:25 +0200

    Dodaj UCUgovori

---
+++ Client/UserControls/UCUgovori.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Client.UserControls
+{
+    partial class UCUgovori
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        private DataGridView dgvUgovori;
+        private TextBox txtKupac;
+        private TextBox txtProdavac;
+        private TextBox txtUgovor;
+        private TextBox txtAutomobil;
+        private Button btnPretrazi;
+        private Button btnDetalji;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvUgovori = new DataGridView();
+            txtKupac = new TextBox();
+            txtProdavac = new TextBox();
+            txtUgovor = new TextBox();
+            txtAutomobil = new TextBox();
+            btnPretrazi = new Button();
+            btnDetalji = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            btnKreiraj = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvUgovori).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvUgovori
+            // 
+            dgvUgovori.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvUgovori.Location = new Point(20, 100);
+            dgvUgovori.Name = "dgvUgovori";
+            dgvUgovori.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvUgovori.Size = new Size(740, 300);
+            dgvUgovori.TabIndex = 0;
+            // 
+            // txtKupac
+            // 
+            txtKupac.Location = new Point(20, 30);
+            txtKupac.Name = "txtKupac";
+            txtKupac.Size = new Size(150, 23);
+            txtKupac.TabIndex = 1;
+            // 
+            // txtProdavac
+            // 
+            txtProdavac.Location = new Point(190, 30);
+            txtProdavac.Name = "txtProdavac";
+            txtProdavac.Size = new Size(150, 23);
+            txtProdavac.TabIndex = 2;
+            // 
+            // txtUgovor
+            // 
+            txtUgovor.Location = new Point(360, 30);
+            txtUgovor.Name = "txtUgovor";
+            txtUgovor.Size = new Size(150, 23);
+            txtUgovor.TabIndex = 3;
+            // 
+            // txtAutomobil
+            // 
+            txtAutomobil.Location = new Point(530, 30);
+            txtAutomobil.Name = "txtAutomobil";
+            txtAutomobil.Size = new Size(150, 23);
+            txtAutomobil.TabIndex = 4;
+            // 
+            // btnPretrazi
+            // 
+            btnPretrazi.Location = new Point(690, 30);
+            btnPretrazi.Name = "btnPretrazi";
+            btnPretrazi.Size = new Size(75, 23);
+            btnPretrazi.TabIndex = 5;
+            btnPretrazi.Text = "Pretraži";
+            btnPretrazi.UseVisualStyleBackColor = true;
+            // 
+            // btnDetalji
+            // 
+            btnDetalji.Enabled = false;
+            btnDetalji.Location = new Point(685, 420);
+            btnDetalji.Name = "btnDetalji";
+            btnDetalji.Size = new Size(75, 23);
+            btnDetalji.TabIndex = 6;
+            btnDetalji.Text = "Detalji";
+            btnDetalji.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(20, 10);
+            label1.Name = "label1";
+            label1.Size = new Size(74, 15);
+            label1.TabIndex = 7;
+            label1.Text = "Email kupca:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(190, 10);
+            label2.Name = "label2";
+            label2.Size = new Size(82, 15);
+            label2.TabIndex = 8;
+            label2.Text = "Ime prodavca:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(360, 10);
+            label3.Name = "label3";
+            label3.Size = new Size(152, 15);
+            label3.TabIndex = 9;
+            label3.Text = "Max. iznos ugovora sa PDV:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(530, 10);
+            label4.Name = "label4";
+            label4.Size = new Size(108, 15);
+            label4.TabIndex = 10;
+            label4.Text = "Model automobila:";
+            // 
+            // btnKreiraj
+            // 
+            btnKreiraj.Location = new Point(593, 420);
+            btnKreiraj.Name = "btnKreiraj";
+            btnKreiraj.Size = new Size(75, 23);
+            btnKreiraj.TabIndex = 11;
+            btnKreiraj.Text = "Kreiraj";
+            btnKreiraj.UseVisualStyleBackColor = true;
+            btnKreiraj.Click += btnKreiraj_Click;
+            // 
+            // UCUgovori
+            // 
+            Controls.Add(btnKreiraj);
+            Controls.Add(dgvUgovori);
+            Controls.Add(txtKupac);
+            Controls.Add(txtProdavac);
+            Controls.Add(txtUgovor);
+            Controls.Add(txtAutomobil);
+            Controls.Add(btnPretrazi);
+            Controls.Add(btnDetalji);
+            Controls.Add(label1);
+            Controls.Add(label2);
+            Controls.Add(label3);
+            Controls.Add(label4);
+            Name = "UCUgovori";
+            Size = new Size(780, 460);
+            ((System.ComponentModel.ISupportInitialize)dgvUgovori).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnKreiraj;
+    }
+}
---
+++ Client/UserControls/UCUgovori.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client.UserControls
+{
+    public partial class UCUgovori : UserControl
+    {
+        public event Action SearchRequested;
+        public event Action<int> DetailsRequested;
+        public event Action AddRequested;
+
+        public string KupacEmail => txtKupac.Text;
+        public string ProdavacIme => txtProdavac.Text;
+        public string MinIznos => txtUgovor.Text;
+        public string AutomobilModel => txtAutomobil.Text;
+        public DataGridView Grid => dgvUgovori;
+
+        public UCUgovori()
+        {
+            InitializeComponent();
+            btnDetalji.Enabled = false;
+            dgvUgovori.SelectionChanged += (s, e) =>
+            {
+                btnDetalji.Enabled = dgvUgovori.SelectedRows.Count > 0;
+            };
+
+            btnPretrazi.Click += btnPretrazi_Click;
+            btnDetalji.Click += btnDetalji_Click;
+        }
+
+
+        public int GetSelectedUgovorId()
+        {
+            if (dgvUgovori.SelectedRows.Count > 0 && dgvUgovori.SelectedRows[0].Cells["IdUgovor"].Value != null)
+            {
+                return Convert.ToInt32(dgvUgovori.SelectedRows[0].Cells["IdUgovor"].Value);
+            }
+            return -1;
+        }
+
+        private void btnPretrazi_Click(object sender, EventArgs e)
+        {
+            SearchRequested?.Invoke();
+        }
+
+        private void btnDetalji_Click(object sender, EventArgs e)
+        {
+            var selectedId = GetSelectedUgovorId();
+            if (selectedId > 0)
+            {
+                DetailsRequested?.Invoke(selectedId);
+            }
+        }
+
+        public void ShowError(string message)
+        {
+            MessageBox.Show(message, "Greška", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        public void ShowInfo(string message)
+        {
+            MessageBox.Show(message, "Informacija", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnKreiraj_Click(object sender, EventArgs e)
+        {
+            AddRequested?.Invoke();
+        }
+    }
+}
