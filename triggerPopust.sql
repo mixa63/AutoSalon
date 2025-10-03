052a49b4033dc17135af76ee97ae58e4ac5a313a
mixa63 <183983866+mixa63@users.noreply.github.com>
2025-10-04 00:25:32 +0200

    Dodaj skripte za kreiranje šeme i inicijalno punjenje baze podacima

---
+++ triggerPopust.sql
@@ -0,0 +1,23 @@
+﻿CREATE TRIGGER trg_SetPopustAndIznos
+ON StavkaUgovora
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    UPDATE s
+    SET 
+        popust = CASE 
+                    WHEN a.tipGoriva = 'elektricni' THEN 0.1
+                    ELSE 0
+                 END,
+        iznos = s.cenaAutomobila * (1 - 
+                   CASE 
+                       WHEN a.tipGoriva = 'elektricni' THEN 0.1
+                       ELSE 0
+                   END)
+    FROM StavkaUgovora s
+    JOIN inserted i ON s.idUgovor = i.idUgovor AND s.rb = i.rb
+    JOIN Automobil a ON i.idAutomobil = a.idAutomobil;
+END;
+
---
+++ triggerUgovor.sql
@@ -0,0 +1,33 @@
+﻿CREATE TRIGGER trg_UpdateUgovorTotals
+ON StavkaUgovora
+AFTER INSERT, UPDATE, DELETE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @idUgovor INT;
+
+    -- Ako INSERT/UPDATE
+    IF EXISTS(SELECT * FROM inserted)
+        SELECT TOP 1 @idUgovor = idUgovor FROM inserted;
+    ELSE
+        -- Ako DELETE
+        SELECT TOP 1 @idUgovor = idUgovor FROM deleted;
+
+    IF @idUgovor IS NOT NULL
+    BEGIN
+        DECLARE @totalBr INT, @totalIznos DECIMAL(18,2), @pdv DECIMAL(5,2);
+
+        SELECT @totalBr = COUNT(*), @totalIznos = ISNULL(SUM(iznos),0)
+        FROM StavkaUgovora
+        WHERE idUgovor = @idUgovor;
+
+        SELECT @pdv = pdv FROM Ugovor WHERE idUgovor = @idUgovor;
+
+        UPDATE Ugovor
+        SET brAutomobila = @totalBr,
+            iznosBezPDV = @totalIznos,
+            iznosSaPDV = @totalIznos * (1 + @pdv)
+        WHERE idUgovor = @idUgovor;
+    END
+END;
