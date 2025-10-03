e880878912e010a4b2d2a2f5c4a71f5c2a057017
mixa63 <183983866+mixa63@users.noreply.github.com>
2025-10-04 01:27:28 +0200

    Dodaj LoginController i MainController, kao i Session singleton

---
+++ Client/GuiController/LoginController.cs
@@ -0,0 +1,51 @@
+﻿using Common.Communication;
+using Common.Domain;
+using Client.Forms;
+using System.DirectoryServices.ActiveDirectory;
+
+namespace Client.GuiController
+{
+    public class LoginController
+    {
+        private FrmLogin frmLogin;
+
+        public LoginController(FrmLogin form)
+        {
+            frmLogin = form;
+            frmLogin.LoginClicked += OnLogin;
+        }
+
+        private void OnLogin(string username, string password)
+        {
+            try
+            {
+                Communication.Instance.Connect();
+                var prodavacRequest = new Prodavac
+                {
+                    Username = username,
+                    Password = password
+                };
+
+                var prodavac = Communication.Instance.SendRequest<Prodavac>(Operation.PrijaviProdavac, prodavacRequest);
+
+                if (prodavac == null)
+                {
+                    throw new Exception("Korisničko ime i šifra nisu ispravni.");
+                }
+                frmLogin.ShowInfo("Korisničko ime i šifra su ispravni.");
+                Session.Instance.Prodavac = prodavac;
+
+                frmLogin.Hide();
+                var frmMain = new FrmMain();
+                var mainController = new MainController(frmMain);
+                frmMain.ShowDialog();
+                frmLogin.Close();
+            }
+            catch (Exception ex)
+            {
+                frmLogin.ShowError(ex.Message);
+                Communication.Instance.Close();
+            }
+        }
+    }
+}
