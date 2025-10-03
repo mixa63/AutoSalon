35f56c5490a5ce2ac18c87132831525af189b10a
mixa63 <183983866+mixa63@users.noreply.github.com>
2025-10-04 00:36:30 +0200

    Dodaj ReadKupacPolymorphic u JsonNetworkSerializer

--- Common/Communication/JsonNetworkSerializer.cs
+++ Common/Communication/JsonNetworkSerializer.cs
@@ -1,25 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Common.Domain;
+using System;
+using System.IO;
 using System.Net.Sockets;
-using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace Common.Communication
 {
     /// <summary>
     /// Omogućava serijalizaciju i deserializaciju objekata u JSON formatu preko mrežnog soketa.
+    /// Pruža metode za slanje i primanje podataka preko <see cref="Socket"/>.
     /// </summary>
     public class JsonNetworkSerializer
     {
         private readonly Socket s;
-        private NetworkStream stream;
-        private StreamReader reader;
-        private StreamWriter writer;
+        private readonly NetworkStream stream;
+        private readonly StreamReader reader;
+        private readonly StreamWriter writer;
 
         /// <summary>
-        /// Kreira instancu JsonNetworkSerializer-a za dati soket.
+        /// Kreira instancu <see cref="JsonNetworkSerializer"/> za dati soket.
         /// </summary>
         /// <param name="s">Soket preko kojeg se komunicira.</param>
         public JsonNetworkSerializer(Socket s)
@@ -36,7 +35,7 @@ namespace Common.Communication
         /// <summary>
         /// Šalje objekat preko toka u JSON formatu.
         /// </summary>
-        /// <param name="z">Objekat koji se šalje.</param>
+        /// <param name="z">Objekat koji se šalje. Može biti bilo koji referentni tip.</param>
         public void Send(object z)
         {
             writer.WriteLine(JsonSerializer.Serialize(z));
@@ -46,33 +45,55 @@ namespace Common.Communication
         /// Prima objekat u JSON formatu i deserializuje ga u dati tip.
         /// </summary>
         /// <typeparam name="T">Tip u koji će JSON biti deserializovan.</typeparam>
-        /// <returns>Deserializovani objekat tipa T.</returns>
+        /// <returns>Deserializovani objekat tipa <typeparamref name="T"/>.</returns>
+        /// <exception cref="IOException">Baca se ako je veza sa soketom prekinuta.</exception>
         public T Receive<T>()
         {
             string json = reader.ReadLine();
+            if (json == null)
+            {
+                throw new IOException("Veza sa klijentom je prekinuta.");
+            }
             return JsonSerializer.Deserialize<T>(json);
         }
 
         /// <summary>
-        /// Deserializuje objekat koji je već primljen u JSON formatu u dati tip.
+        /// Deserializuje već primljeni JSON objekat u dati tip.
         /// </summary>
         /// <typeparam name="T">Tip u koji će podaci biti deserializovani. Mora biti referentni tip.</typeparam>
-        /// <param name="podaci">Objekat koji treba deserializovati.</param>
-        /// <returns>Deserializovani objekat tipa T, ili null ako je ulazni objekat null.</returns>
-        public T ReadType<T>(object podaci) where T : class
+        /// <param name="podaci">JSON objekat koji treba deserializovati.</param>
+        /// <returns>Deserializovani objekat tipa <typeparamref name="T"/> ili <c>default(T)</c> ako je ulaz <c>null</c>.</returns>
+        public T ReadType<T>(object podaci)
         {
-            return podaci == null ? null : JsonSerializer.Deserialize<T>((JsonElement)podaci);
+            if (podaci == null)
+                return default(T);
+
+            JsonElement element = (JsonElement)podaci;
+            return element.Deserialize<T>();
         }
 
         /// <summary>
-        /// Zatvara sve tokove i mrežni stream.
+        /// Deserializuje <see cref="Kupac"/> objekat u polimorfnom obliku na osnovu tipa.
         /// </summary>
-        public void Close()
+        /// <param name="podaci">JSON objekat koji sadrži informacije o kupcu.</param>
+        /// <returns>
+        /// Deserializovani <see cref="Kupac"/> ili njegov derivat (<see cref="FizickoLice"/> ili <see cref="PravnoLice"/>)
+        /// na osnovu vrednosti <c>TableName</c> u JSON-u.
+        /// Vraća <c>null</c> ako je ulaz <c>null</c>.
+        /// </returns>
+        public Kupac ReadKupacPolymorphic(object podaci)
         {
-            stream.Close();
-            reader.Close();
-            writer.Close();
+            if (podaci == null) return null;
+
+            JsonElement element = (JsonElement)podaci;
+            string tableName = element.GetProperty("TableName").GetString();
+
+            if (tableName == "FizickoLice")
+                return element.Deserialize<FizickoLice>();
+            else if (tableName == "PravnoLice")
+                return element.Deserialize<PravnoLice>();
+            else
+                return element.Deserialize<Kupac>();
         }
     }
 }
-
