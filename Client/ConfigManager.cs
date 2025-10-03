962b42944fa092c58172a2139db1088f4f14747d
mixa63 <183983866+mixa63@users.noreply.github.com>
2025-10-04 01:19:44 +0200

    Dodaj ConfigManager klasu

---
+++ Client/ConfigManager.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal static class ConfigManager
+    {
+        private static readonly IConfigurationRoot _configuration;
+        public static AppConfig AppConfig { get; }
+
+        static ConfigManager()
+        {
+            _configuration = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("config.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            AppConfig = new AppConfig
+            {
+                Ip = _configuration["AppConfig:Ip"],
+                Port = int.Parse(_configuration["AppConfig:Port"] ?? "7000")
+            };
+        }
+    }
+}
